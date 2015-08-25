a8a61e8f96234c305d9bb9c815bc79aea1c7939a
nickzuck <nikhil1945.singh@gmail.com>
2015-08-25 22:30:50 +0530

    Used function ...still NOT WORKING

--- longest_common_subsequence.cpp
+++ longest_common_subsequence.cpp
@@ -2,46 +2,46 @@
 #include<algorithm>
 #include<cstring>
 
-#define MAX 1002
+#define MAX 10002
 
 using namespace std ;
 
+int calculate (char a [], char b[] , int lena , int lenb)
+{
+	//cout << a << " " << b ;
+	int lcs[lena+1][lenb+1] ;
+	
+	for (int i = 0 ;i<= lena ; i++){
+		for (int j = 0 ; j<= lenb ; j++){
+			cout << a[i] << " " << b[j] << endl;
+			if (i == 0 || j == 0)
+				lcs[i][j] = 0 ; 
+			else if (a[i-1] == b[i-1])
+				lcs[i][j] = lcs[i-1][j-1] + 1 ;
+			else
+				lcs[i][j] = max(lcs[i-1][j] , lcs[i][j-1]) ;
+		}
+	}
+return lcs[lena][lenb] ;
+}
+
+int max(int a , int b)
+{
+	
+	return (a > b)? a : b;
+}
+
 int main ()
 {
-        int lena , lenb , i , j ; 
-        char a[MAX] , b[MAX] ; 
-        
-        cin>> a ; 
-        cin >> b ; 
-
-
-        lena = strlen(a) ; 
-        lenb = strlen(b) ;
-
-        int lcs[lena][lenb] ;  
-
-        for (i = 0 ;i<=lena ; i++) {
-                for (j = 0 ; j<=lenb ; j++){
-			
-                        if (i == 0 || j == 0){
-                                lcs[i][j] = 0 ;
-                        }
-                        else {
-                      		
-                                cout << lcs[i-1][j] << "  "<< lcs[i][j-1] << endl;
-                                lcs[i][j] = max(lcs[i][j-1] ,lcs[i-1][j]) ; 
-                               
-				//cout << a[i-1] <<"  "<< b[j-1] << endl;
-                                if (a[i-1] == b[j-1]){
-                                        lcs[i][j] = max (lcs[i-1][j-1] +1 , lcs[i][j]) ;
-                                }
-                                cout << lcs[i][j] << endl ;
-                                       
-                        }
-                }
-        }
-
-	cout << "ans = " << lcs[lena][lenb] << "\n" ;
-
+	char a[MAX], b[MAX] ; 
+	int lena , lenb ; 
+	
+	cin >> a ;
+	cin >> b;
+	
+	lena = strlen (a) ; 
+	lenb = strlen (b) ;
+	
+	cout << calculate (a , b , lena , lenb) << endl;
 return 0 ; 
 }
