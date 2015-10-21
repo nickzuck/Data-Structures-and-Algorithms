83aa756bec7db8a7f10b502a0666391662fd9642
nickzuck <nikhil1945.singh@gmail.com>
2015-10-21 19:30:22 +0530

    this MAKE-HEAPIFY  works good

--- heapsort.cpp
+++ heapsort.cpp
@@ -14,29 +14,25 @@ inline void swap(int &a , int &b){
 	a = temp ;
 }
 
-void max_heapify(int *arr , int i , int n ){
-	int left , right, largest  ; 
-	left = 2*i ; 
-	right = left +1 ;
-	//largest = i ;
-	//for left-subtree
-	if(left <= n && arr[left] > arr[i]){
-		largest = left ; 
-	}
-	else {
-		largest = i ;
-	}
-	
-	//for right-subtree
-	if(right <= n && arr[right]> arr[largest]){
-		largest = right ; 
-	}
-
-	if (largest != i){
-		swap(arr[i] , arr[largest]) ;
-	}
-	max_heapify(arr , largest , n) ;
-
+void max_heapify(int *a, int i, int n)
+{
+    int j, temp;
+    temp = a[i];
+    j = 2*i;
+    while (j <= n)
+    {
+        if (j < n && a[j+1] > a[j])
+            j = j+1;
+        if (temp > a[j])
+            break;
+        else if (temp <= a[j])
+        {
+            a[j/2] = a[j];
+            j = 2*j;
+        }
+    }
+    a[j/2] = temp;
+    return;
 }
 
 int main ()
