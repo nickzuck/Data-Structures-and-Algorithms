dd5f04ccbf6c46ba3e15eb473c19a60e49d72dd9
nickzuck <nikhil1945.singh@gmail.com>
2015-10-25 02:50:36 +0530

    Completed

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -7,9 +7,28 @@
 using namespace std; 
 
 
-void radix_sort(int *arr , int n , int digits , int radix = 10) //radix = 10 for decimal numbers
+
+//Radix SORT
+/*radix = 10 for decimal numbers you may pass another variable if you want to change the radix*/
+void radix_sort(int *a , int n , int digits , int radix = 10) 
 {
-	queue <int> radix_queues ; 	
+	queue <int> radix_queues[radix] ; 
+	for(int i = 0, factor = 1; i < digits; factor *= radix, i++)
+	{
+		for(int j = 0; j < n; j++)
+			radix_queues[((a[j]/factor)%radix)].push(a[j]);
+		int k = 0;
+		for(int j = 0; j < radix; j++)
+		{
+			while(!radix_queues[j].empty())
+			{
+				a[k] = radix_queues[j].front();
+				radix_queues[j].pop();
+				k++;
+			}
+		}
+		cout << endl ;
+	}	
 }
 
 
@@ -41,5 +60,8 @@ int main ()
 	int digits = getmaxdigit(maximum);
 	cout << digits << endl;
 	radix_sort(arr , n, digits );
+	cout << "sorted array\n";
+	for (int i =0 ;i <n ;i++)
+		cout << arr[i] << " ";
 return 0 ;
 }
