4d6747ec16c4a695e4cca6c826b59eabc8c3e546
nickzuck <nikhil1945.singh@gmail.com>
2015-12-08 21:21:21 +0530

    Build Tree completed successfully

---
+++ segmentTree.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring> //For memset
+
+#define MAX  101
+using namespace std ;
+
+int arr[MAX];
+int tree[2*MAX + 1];
+
+//function definition
+void update();
+void query();
+void build_tree(int , int , int );
+
+//function to find the middle value
+inline int getMid (int a , int b){
+    return (a+b)/2;
+}
+
+//Build segment tree
+void build_tree(int node , int a , int b){
+    if (a > b){
+        return ; // out of range
+    }
+    if(a == b){
+        tree[node] = arr[a];
+        return ;
+    }
+    int mid = getMid(a , b);
+    build_tree(node*2 , a , mid);
+    build_tree(node*2+1 , mid+1 , b );
+    tree[node] = tree[node*2] + tree[node*2 + 1] ;
+}
+
+//Driver Program
+int main()
+{
+    int n ,i ; 
+    cout << "Enter the number of elements in the array\t";
+    cin >> n ;
+
+    //Input
+    for(i = 0 ;i<n;i++){
+        cin >> arr[i];
+    }
+
+    //initializing the arrays 
+    memset(tree , 0 , sizeof(tree));
+    //Build Tree
+    build_tree(1 , 0 , n-1);
+
+    cout << "After tree build\n";
+    for(i = 1 ; i <= 2*n -1; i++)
+        cout << tree[i] << " " ;
+return 0 ;
+}
