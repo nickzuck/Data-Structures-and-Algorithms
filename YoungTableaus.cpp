fec42a153c9a91dbc6d2bbd00117e0bbaa054551
nickzuck <nikhil1945.singh@gmail.com>
2016-01-17 21:13:11 +0530

    Able to convert the array into Young Tableaus

--- YoungTableaus.cpp
+++ YoungTableaus.cpp
@@ -2,10 +2,11 @@
 #include<cmath>
 #include<algorithm>
 
-#define MAX  65535
+#define INF  65535
 
 void youngify(int i, int j);
 int young[100][100] ;
+int arr[100];
 int m , n ; 
 using namespace std ;
 
@@ -15,46 +16,60 @@ int extract_min(){
     youngify( 0, 0);
 }
 
-void youngify(int i, int j){
-    int x, y ; 
-    x  = i ; 
-    y = j ;
-    int temp ;
-    if (i +1 <= m){
-        if(young[i][j] > young[i+1][j]){
-            x  = i+1 ;
-            y = j ;
-        }
-    }
-    if(j+1 <n){
-        if(young[x][y] > young[i][j+1]){
-            x = i ; 
-            y = j+1; 
 
-        }
+void youngify(int i, int j)
+{
+    int downVal  = (i+1 < m)? young[i+1][j]: INF;
+    int rightVal = (j+1 < n)? young[i][j+1]: INF;
+ 
+    if (downVal==INF && rightVal==INF)
+        return;
+ 
+    if (downVal < rightVal)
+    {
+        young[i][j] = downVal;
+        young[i+1][j] = INF;
+        youngify(i+1, j);
     }
-
-    if(x != i || y != j){
-        temp = young[x][y] ;
-        young[x][y] = young[i][j] ; 
-        young[i][j] = temp ; 
-        youngify(x, y);
+    else
+    {
+        young[i][j] = rightVal;
+        young[i][j+1] = INF;
+        youngify(i, j+1);
     }
 }
 
+void initialize(){
+    int i , j, k;
+    k = m*n-1 ;
+    for(i = m-1; i>=0 ; i--){
+        for(j = n-1; j>=0; j--){
+            young[j][i] = arr[k--];   
+        }
+    }
+}
 // Insertion
-//void insert_key(
+void insert_key(int i, int j){
+    
+}
 int main()
 {
     int i, j ;
     cin >> m >> n ; 
-    for(i = 0 ; i<m ; i++){
-        for(j = 0 ; j<n ; j++){
-            cin >> young[i][j];
-        }
+    for(i = 0 ; i<m*n ; i++){
+        cin >> arr[i];
     }
 
+    sort(arr, arr+m*n) ;
+    cout << "After sorting\n" ; 
+    for(i = 0 ; i<m*n ; i++)
+        cout << arr[i] << " ";
+    cout << endl ;
+
+    initialize();
+
     youngify(m,n);
+
     for(i = 0 ;i<m ; i++){
         for(j = 0 ;j<n ; j++){
             cout << young[i][j] << " " ; 
